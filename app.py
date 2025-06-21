16dafdac70984b4e2e623cacb04de41b8975d5ee
Shayane Katchera <45144374+shayanekat@users.noreply.github.com>
2025-06-21 14:09:47 +0200

    ajout d'un editeur de texte comme premiere application

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ import os
 
 import werkzeug.security
 from dotenv import load_dotenv
-from flask import Flask, redirect, render_template_string, request, url_for
+from flask import (Flask, redirect, render_template, render_template_string,
+                   request, url_for)
 
 load_dotenv()  # Charge les variables du fichier .env
 
@@ -39,9 +40,23 @@ def dashboard():
         dashboard_page = file.read()
     return render_template_string(dashboard_page)
 
-@app.route('/app1')
+@app.route('/app1', methods=['GET', 'POST'])
 def app1():
-    return "<h3>Ceci est ton application 1 (à coder plus tard)</h3>"
+    file_path = 'apps/editor/note.txt'
+    message = None
+
+    if request.method == 'POST':
+        content = request.form['content']
+        with open(file_path, 'w', encoding='utf-8') as f:
+            f.write(content)
+        message = "✔️ Fichier sauvegardé avec succès."
+
+    # Lire le fichier à chaque affichage
+    with open(file_path, 'r', encoding='utf-8') as f:
+        content = f.read()
+
+    return render_template('app1_editor.html', content=content, message=message)
+
 
 @app.route('/app2')
 def app2():
